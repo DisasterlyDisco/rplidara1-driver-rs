89d9280ab3c203c37a94761cfe72b0c021de00b8
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2025-03-25 19:20:58 +0100

    Driver constructor

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,8 @@ mod rplidar;
 use crate::rplidar::driver::get_info;
 
 fn main() {
-    let mut port = serialport::new("/dev/ttyUSB0", 115200)
-        .open()
-        .expect("Unable to open");
-    port.set_timeout(std::time::Duration::from_secs(10))
-        .expect("Unable to set timeout");
-    let read_buf = get_info(&mut port).expect("Read failed");
+    let mut driver = rplidar::driver::new_connection();
+    let read_buf = get_info(&mut driver).expect("Read failed");
     // Print read_buf as hex:
     for byte in &read_buf {
         print!("{:02X} ", byte);
--- src/rplidar/driver.rs
+++ src/rplidar/driver.rs
@@ -1,5 +1,14 @@
 use serialport::{Error, SerialPort};
 
+pub fn new_connection() -> Box<dyn SerialPort> {
+    let mut port = serialport::new("/dev/ttyUSB0", 115200)
+        .open()
+        .expect("Unable to open");
+    port.set_timeout(std::time::Duration::from_millis(250))
+        .expect("Unable to set timeout");
+    port
+}
+
 pub fn get_info(port: &mut Box<dyn SerialPort>) -> Result<[u8; 27], Error> {
     const MSG: [u8; 2] = [0xA5, 0x50];
     let mut read_buf: [u8; 27] = [0; 27];
